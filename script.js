b84b626d31058afc3f0a75f938ad4e932056b807
ClevelandIII <kingslime108@gmail.com>
2024-10-08 11:30:50 -0700

    Highlight change

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ let currentTime = now.toLocaleTimeString("en-US", {
 let time = document.getElementById("time");
 
 time.innerHTML = `${currentTime}`;
+
+function setTime() {
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+
+    let time = document.getElementById("time");
+
+    time.innerHTML = `${currentTime}`;
+}
+
+setInterval(setTime, 1000);
