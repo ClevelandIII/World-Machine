0c9a8e8c7f00d589b77b211f6a6c38de4cb75ecc
ClevelandIII <kingslime108@gmail.com>
2024-10-07 23:57:29 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Basic time format from GeeksToGeeks
+
+let now = new Date();
+let currentTime = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+});
+
+let time = document.getElementById("time");
+
+time.innerHTML = `${currentTime}`;
